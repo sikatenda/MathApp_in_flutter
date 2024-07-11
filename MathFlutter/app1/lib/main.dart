1401fab027acf2d2cfa7f4c75d9ccabcf48766e2
Jacques Feruzi <jferuzi019@gmail.com>
2024-07-11 17:40:27 +0200

    button navigator

--- MathFlutter/app1/lib/main.dart
+++ MathFlutter/app1/lib/main.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:app1/power.dart';
 
 void main() {
   runApp(const MyWidget());
 }
 
-class MyWidget extends StatelessWidget {
+class MyWidget extends StatefulWidget {
   const MyWidget({super.key});
 
-  //int? number;
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
 
-  //Addition a = Addition();
-
-  //void increase() {
-  // int? num1;
-  // int? number = (num1! + 1);
-  //}
-
-  //Addition a = Addition();
-
+class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,27 +22,37 @@ class MyWidget extends StatelessWidget {
           title: const Center(
             child: Text("FERUZI"),
           ),
-          backgroundColor: Color.fromARGB(255, 77, 225, 84),
+          backgroundColor: const Color.fromARGB(255, 77, 225, 84),
         ),
         body: SafeArea(
           child: Center(
             child: Container(
               padding: const EdgeInsets.all(12),
-              margin: EdgeInsets.all(8),
+              margin: const EdgeInsets.all(8),
               color: Colors.brown,
               width: double.infinity,
               height: double.infinity,
               child: Column(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
-                    child: const Text("You are  year(s) old !"),
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                    child: Text("You are  year(s) old !"),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
                     child: Text("Jacques Feruzi"),
                   ),
-                  FilledButton(onPressed: () {}, child: const Text("Process")),
+                  Builder(builder: (context) {
+                    return ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const MyPower()),
+                          );
+                        },
+                        child: const Text("Power Page"));
+                  }),
                 ],
               ),
             ),
@@ -55,20 +60,5 @@ class MyWidget extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-class Addition {
-  //properties
-  int? number;
-
-  //constructor
-  Addition() {
-    this.number = number;
-  }
-
-  //Method
-  void add() {
-    number = number! + 1;
   }
 }
--- MathFlutter/app1/lib/power.dart
+++ MathFlutter/app1/lib/power.dart
@@ -1,22 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:app1/main.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+class MyPower extends StatelessWidget {
+  const MyPower({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Center(
-            child: Text("FERUZI"),
+        //appBar: AppBar(
+        // title: const Center(
+        //   child: Text("FERUZI"),
+        // ),
+        //  backgroundColor: Color.fromARGB(255, 77, 225, 84),
+        // ),
+        body: SafeArea(
+          child: Center(
+            child: Container(
+              padding: const EdgeInsets.all(12),
+              margin: const EdgeInsets.all(8),
+              color: Colors.brown,
+              width: double.infinity,
+              height: double.infinity,
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                    child: Text("You are  year(s) old !"),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                    child: Text("Jacques Feruzi"),
+                  ),
+                  Builder(builder: (context) {
+                    return ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const MyWidget()),
+                          );
+                        },
+                        child: const Text("Return"));
+                  }),
+                ],
+              ),
+            ),
           ),
-          backgroundColor: Colors.cyan[300],
         ),
-        body: const Center(
-          child: Image(
-            image: AssetImage("images/flutterflow.jpg"),
-          ),
-        ),
-        backgroundColor: Colors.grey[400],
       ),
-    ),
-  );
+    );
+  }
 }
