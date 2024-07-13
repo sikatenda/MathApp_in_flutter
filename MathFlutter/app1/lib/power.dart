483dd9366091cb9e5d04e8fb5bd046fe60e7f1e8
Jacques Feruzi <jferuzi019@gmail.com>
2024-07-13 16:40:43 +0200

    square root page

--- MathFlutter/app1/lib/power.dart
+++ MathFlutter/app1/lib/power.dart
@@ -1,30 +1,21 @@
-import 'dart:ffi';
-import 'dart:nativewrappers/_internal/vm/lib/core_patch.dart';
-import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
-
 import 'package:flutter/material.dart';
 import 'package:app1/main.dart';
+import 'package:flutter/services.dart';
 
 class MyPower extends StatefulWidget {
   const MyPower({super.key});
 
   @override
-  State<MyPower> createState() => _MyPowerState();
+  State<MyPower> createState() => _MySquareState();
 }
 
-class _MyPowerState extends State<MyPower> {
-  int num1 = 0;
-  int num2 = 0;
+class _MySquareState extends State<MyPower> {
+  final textController1 = TextEditingController();
+  final textController2 = TextEditingController();
 
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
-  Widget? _buildNum1() {
-    return null;
-  }
-
-  Widget? _buildNum2() {
-    return null;
-  }
+  double exponent = 0.0;
+  double base = 0.0;
+  double result = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -41,28 +32,108 @@ class _MyPowerState extends State<MyPower> {
           child: Center(
             child: Container(
               padding: const EdgeInsets.all(12),
-              margin: const EdgeInsets.all(8),
+              margin: const EdgeInsets.all(6),
               color: Colors.brown,
-              //width: double.infinity,
-              //height: double.infinity,
-              child: const Column(
+              width: double.infinity,
+              height: double.infinity,
+              child: Column(
                 children: [
-                  Padding(
+                  const Padding(
                     padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
-                    child: Text("Power !"),
+                    child: Text("Get the power of a number  !",
+                        style: TextStyle(color: Colors.black, fontSize: 18)),
                   ),
                   Padding(
-                    padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
-                    child: Text("Jacques Feruzi"),
+                    padding: const EdgeInsets.fromLTRB(10, 20, 0, 40),
+                    child: Text("$result",
+                        style:
+                            const TextStyle(color: Colors.black, fontSize: 35)),
                   ),
-                  Form(
-                      child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      _buildNum1(),
-                      _buildNum2(),
-                    ],
-                  ))
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 10),
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          height: 40.0,
+                          width: 60.0,
+                          child: TextField(
+                              controller: textController1,
+                              keyboardType: TextInputType.number,
+                              inputFormatters: [
+                                FilteringTextInputFormatter.digitsOnly
+                              ],
+                              style: const TextStyle(fontSize: 21),
+                              decoration: const InputDecoration(
+                                filled: true,
+                                hintText: "00",
+                                border: OutlineInputBorder(),
+                              )),
+                        ),
+                        SizedBox(
+                          height: 40.0,
+                          width: 60.0,
+                          child: TextField(
+                              controller: textController2,
+                              keyboardType: TextInputType.number,
+                              inputFormatters: [
+                                FilteringTextInputFormatter.digitsOnly
+                              ],
+                              style: const TextStyle(fontSize: 21),
+                              decoration: const InputDecoration(
+                                filled: true,
+                                hintText: "0.0",
+                                border: OutlineInputBorder(),
+                              )),
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(
+                              padding: const EdgeInsets.fromLTRB(50, 0, 0, 0)),
+                          onPressed: () {
+                            setState(() {
+                              textController1.clear();
+                              textController2.clear();
+                              result = 0.0;
+                            });
+                          },
+                          child: const Text(
+                            'Clear',
+                            style: TextStyle(color: Colors.black, fontSize: 16),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Builder(builder: (context) {
+                    return ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black,
+                          padding: const EdgeInsets.all(16),
+                          fixedSize: const Size.fromWidth(250),
+                          foregroundColor: Colors.white,
+                        ),
+                        onPressed: () {},
+                        child: const Text("Process"));
+                  }),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Builder(builder: (context) {
+                      return ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            padding: const EdgeInsets.all(16),
+                            fixedSize: const Size.fromWidth(250),
+                            foregroundColor: Colors.white,
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const MyWidget()),
+                            );
+                          },
+                          child: const Text("Back"));
+                    }),
+                  )
                 ],
               ),
             ),
--- MathFlutter/app1/lib/squareRoot.dart
+++ MathFlutter/app1/lib/squareRoot.dart
@@ -1,8 +1,21 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:app1/main.dart';
+import 'package:flutter/services.dart';
 
-class MySquare extends StatelessWidget {
+class MySquare extends StatefulWidget {
   const MySquare({super.key});
+
+  @override
+  State<MySquare> createState() => _MySquareState();
+}
+
+class _MySquareState extends State<MySquare> {
+  final textController1 = TextEditingController();
+
+  double result = 0.0;
+  double sqr = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +32,7 @@ class MySquare extends StatelessWidget {
           child: Center(
             child: Container(
               padding: const EdgeInsets.all(12),
-              margin: const EdgeInsets.all(8),
+              margin: const EdgeInsets.all(6),
               color: Colors.brown,
               width: double.infinity,
               height: double.infinity,
@@ -27,29 +40,88 @@ class MySquare extends StatelessWidget {
                 children: [
                   const Padding(
                     padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
-                    child: Text("Square root  !"),
+                    child: Text("Calculate a square root  !",
+                        style: TextStyle(color: Colors.black, fontSize: 18)),
                   ),
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
-                    child: Text("Jacques Feruzi"),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 40, 0, 40),
+                    child: Text("$result",
+                        style:
+                            const TextStyle(color: Colors.black, fontSize: 21)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 10),
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          height: 60.0,
+                          width: 60.0,
+                          child: TextField(
+                              controller: textController1,
+                              keyboardType: TextInputType.number,
+                              inputFormatters: [
+                                FilteringTextInputFormatter.digitsOnly
+                              ],
+                              style: const TextStyle(fontSize: 21),
+                              decoration: const InputDecoration(
+                                filled: true,
+                                hintText: "00",
+                                border: OutlineInputBorder(),
+                              )),
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(
+                              padding: const EdgeInsets.fromLTRB(100, 0, 0, 0)),
+                          onPressed: () {
+                            setState(() {
+                              textController1.clear();
+                              result = 0.0;
+                            });
+                          },
+                          child: const Text(
+                            'Clear',
+                            style: TextStyle(color: Colors.black, fontSize: 16),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                   Builder(builder: (context) {
                     return ElevatedButton(
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.black,
                           padding: const EdgeInsets.all(16),
-                          fixedSize: Size.fromWidth(250),
+                          fixedSize: const Size.fromWidth(250),
                           foregroundColor: Colors.white,
                         ),
                         onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const MyWidget()),
-                          );
+                          setState(() {
+                            sqr = double.parse(textController1.text);
+                            result = sqrt(sqr);
+                          });
                         },
-                        child: const Text("Return"));
+                        child: const Text("Process"));
                   }),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Builder(builder: (context) {
+                      return ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            padding: const EdgeInsets.all(16),
+                            fixedSize: const Size.fromWidth(250),
+                            foregroundColor: Colors.white,
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const MyWidget()),
+                            );
+                          },
+                          child: const Text("Back"));
+                    }),
+                  )
                 ],
               ),
             ),
